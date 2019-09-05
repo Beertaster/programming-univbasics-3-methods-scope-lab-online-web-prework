3374968be8f0dc10f797327b0f89c166dd8ac2b4
William Stevens <wstevens.hornet@gmail.com>
2019-09-05 23:30:39 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,5 +1,4 @@
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   phrase = "it's-a me, Mario!"
   puts phrase
 end
@@ -7,4 +6,11 @@ end
 def toadstool
   status = "Thank You Mario! But Our Princess Is In Another Castle!"
   puts status
+end
+
+def link 
+  puts "It's Dangerous To Go Alone! Take This."
+end
+any_phrase(phrase)
+  puts phrase
 end
